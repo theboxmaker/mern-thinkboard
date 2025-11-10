1c9edc4b0a55086a1519ab680ce51a0cb14b1aa9
theboxmaker <ztucker99@gmail.com>
2025-11-10 16:56:13 -0500

    prepared for deployment

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(5, "10 s")
+    limiter: Ratelimit.slidingWindow(100, "10 s")
 });
 
 export default ratelimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,13 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(rateLimiter)
@@ -16,10 +18,23 @@ app.use((req, res, next) => {
     console.log("We just got a new request");
     next();
 })
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }
+    ))
+}
 
-app.use(cors())
-app.use("/api/notes", notesRoutes);
 app.use(rateLimiter);
+app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
